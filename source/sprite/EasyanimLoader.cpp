74937315253c3885c3623ff781d47570438bffae
zz <zhuguang@ejoy.com>
2016-12-05 18:23:02 +0800

    [FIXED] load with lerp

--- source/sprite/EasyanimLoader.cpp
+++ source/sprite/EasyanimLoader.cpp
@@ -5,6 +5,9 @@
 
 #include <sprite2/AnimSymbol.h>
 #include <sprite2/S2_Sprite.h>
+#include <sprite2/LerpCircle.h>
+#include <sprite2/LerpSpiral.h>
+#include <sprite2/AnimLerp.h>
 #include <simp/NodeAnimation.h>
 #include <simp/from_int.h>
 
@@ -94,6 +97,25 @@ void EasyAnimLoader::LoadLayers(const Json::Value& val, const std::string& dir)
 			s2::AnimSymbol::Frame* dst_frame = new s2::AnimSymbol::Frame;
 			dst_frame->index = frame_val["time"].asInt();
 			dst_frame->tween = frame_val["tween"].asBool();
+			for (int i = 0, n = frame_val["lerp"].size(); i < n; ++i)
+			{
+				s2::AnimLerp::SprData key = s2::AnimLerp::SprData(frame_val["lerp"][i]["key"].asInt());
+				const Json::Value& val = frame_val["lerp"][i]["val"];
+				if (val["type"].asString() == "circle")
+				{
+					float scale = val["scale"].asInt() * 0.01f;
+					s2::LerpCircle* circle = new s2::LerpCircle(scale);
+					dst_frame->lerps.push_back(std::make_pair(key, circle));
+				}
+				else if (val["type"].asString() == "spiral") 
+				{
+					float begin = val["angle_begin"].asInt() * SM_DEG_TO_RAD,
+						end   = val["angle_end"].asInt() * SM_DEG_TO_RAD;
+					float scale = val["scale"].asInt() * 0.01f;
+					s2::LerpSpiral* spiral = new s2::LerpSpiral(begin, end, scale);
+					dst_frame->lerps.push_back(std::make_pair(key, spiral));
+				}
+			}
 			int actor_n = frame_val["actor"].size();
 			dst_frame->sprs.reserve(actor_n);
 			for (int actor = 0; actor < actor_n; ++actor)
