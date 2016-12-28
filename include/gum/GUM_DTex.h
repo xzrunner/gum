f82696194899a76721972f0b09e11580cc7bb51e
zz <zhuguang@ejoy.com>
2016-12-28 17:46:01 +0800

    rm dtex1

--- include/gum/GUM_DTex.h
+++ include/gum/GUM_DTex.h
@@ -1,46 +1,57 @@
-//#ifndef _GUM_DTEX_H_
-//#define _GUM_DTEX_H_
-//
-//#include <CU_Singleton.h>
-//
-//#include <string>
-//#include <map>
-//
-//struct dtex_cg;
-//
-//namespace s2 { class Texture; }
-//
-//namespace gum
-//{
-//
-//class DTex
-//{
-//public:
-//	void InitHook(void (*draw_begin)(), void (*draw_end)());
-//
-//	void LoadBegin();
-//	int Load(const std::string& filepath, const s2::Texture* tex);
-//	void LoadEnd();
-//
-//	float* Query(const std::string& filepath, const s2::Texture* tex, int* id);
-//
-//	void Clear();
-//
-//	dtex_cg* GetDtexCG();
-//
-//	void OnSize(int w, int h);
-//
-//	void Update();
-//
-//	void DebugDraw() const;
-//
-//protected:
-//	std::map<std::string, int> m_map_filepath;
-//
-//	SINGLETON_DECLARATION(DTex)
-//
-//}; // DTex
-//
-//}
-//
-//#endif // _GUM_DTEX_H_
+#ifndef _GUM_DTEX_H_
+#define _GUM_DTEX_H_
+
+#include <CU_Singleton.h>
+
+#include <string>
+#include <map>
+#include <set>
+
+#include <stdint.h>
+
+namespace dtex { class CacheSymbol; }
+
+namespace gum
+{
+
+class DTex
+{
+public:
+	void InitHook(void (*draw_begin)(), void (*draw_end)());
+
+	void CreatePkg(int pkg_id);
+
+	void LoadSymStart();
+	void LoadSymbol(const std::string& filepath, int tex_id, int tex_w, int tex_h);
+	void LoadSymFinish();
+	const float* QuerySymbol(const std::string& filepath, int* tex_id) const;
+
+	void Clear();
+
+	void DebugDraw() const;
+
+private:
+	struct Filepath2ID
+	{
+		uint32_t Add(const std::string& filepath);
+		uint32_t Query(const std::string& filepath) const;
+
+		static uint32_t HashStr(const std::string& str);
+
+		std::map<std::string, int> map_filepath;
+		std::set<int> set_id;
+
+		std::map<std::string, int> conflict;
+	};
+
+private:
+	dtex::CacheSymbol* m_c2;
+	Filepath2ID m_file2id;
+
+	SINGLETON_DECLARATION(DTex)
+
+}; // DTex
+
+}
+
+#endif // _GUM_DTEX_H_
--- source/adapter/GUM_DTex.cpp
+++ source/adapter/GUM_DTex.cpp
@@ -1,323 +1,418 @@
-//#include "GUM_DTex.h"
-//#include "RenderContext.h"
-//
-//#include <unirender/RenderContext.h>
-//
-//#include <sprite2/S2_RVG.h>
-//#include <sprite2/RenderScissor.h>
-//#include <sprite2/RenderCtxStack.h>
-//#include <sprite2/Texture.h>
-//
-//#include <assert.h>
-//
-//namespace gum
-//{
-//
-//SINGLETON_DEFINITION(DTex)
-//
-//static void (*DRAW_BEGIN)() = NULL;
-//static void (*DRAW_END)() = NULL;
-//
-////////////////////////////////////////////////////////////////////////////
-//
-//static void _program(int n) 
-//{
-//// 	switch (n) 
-//// 	{
-//// 	case DTEX_PROGRAM_NULL:
-//// //		ShaderMgr::Instance()->SetShader(ShaderMgr::COUNT);
-//// 		break;
-//// 	case DTEX_PROGRAM_NORMAL:
-//// 		sl::ShaderMgr::Instance()->SetShader(sl::SPRITE2);
-//// 		break;
-//// 	default:
-//// 		assert(0);
-//// 	}
-//}
-//
-//static void _blend(int mode)
-//{
-//	assert(mode == 0);
-////	ShaderMgr::Instance()->SetBlendMode(0);
-//}
-//
-//static void _set_texture(int id)
-//{
-//	sl::ShaderMgr::Instance()->GetContext()->BindTexture(id, 0);
-//}
-//
-//static int _get_texture()
-//{
-//	return 	sl::ShaderMgr::Instance()->GetContext()->GetCurrTexture();
-//}
-//
-//static void _set_target(int id)
-//{
-//	sl::ShaderMgr::Instance()->GetContext()->BindRenderTarget(id);
-//}
-//
-//static int _get_target()
-//{
-//	return sl::ShaderMgr::Instance()->GetContext()->GetCurrRenderTarget();
-//}
-//
-//static void _draw_begin() 
-//{
-//	if (DRAW_BEGIN) {
-//		DRAW_BEGIN();
-//	} else {
-//		s2::RenderCtxStack::Instance()->Push(s2::RenderCtx(2, 2, 0, 0), false);
-//	}
-//
-//	sl::ShaderMgr* sl_mgr = sl::ShaderMgr::Instance();
-//	sl_mgr->SetShader(sl::SPRITE2);
-//	sl::Sprite2Shader* sl_shader = static_cast<sl::Sprite2Shader*>(sl_mgr->GetShader());
-//	sl_shader->SetColor(0xffffffff, 0);
-//	sl_shader->SetColorMap(0x000000ff, 0x0000ff00, 0x00ff0000);
-//}
-//
-//static void _draw(const float vb[16], int texid)
-//{
-//	sm::vec2 vertices[4], texcoords[4];
-//	for (int i = 0; i < 4; ++i) {
-//		vertices[i].x  = vb[i * 4];
-//		vertices[i].y  = vb[i * 4 + 1];
-//		texcoords[i].x = vb[i * 4 + 2];
-//		texcoords[i].y = vb[i * 4 + 3];
-//	}
-//
-//	sl::ShaderMgr* sl_mgr = sl::ShaderMgr::Instance();
-//	switch (sl_mgr->GetShaderType()) 
-//	{
-//	case sl::SPRITE2:
-//		{
-//			sl::Sprite2Shader* shader = static_cast<sl::Sprite2Shader*>(sl_mgr->GetShader());
-//			shader->Draw(&vertices[0].x, &texcoords[0].x, texid);
-//		}
-//		break;
-//	case sl::FILTER:
-//		{
-//			sl::FilterShader* shader = static_cast<sl::FilterShader*>(sl_mgr->GetShader());
-//			shader->Draw(&vertices[0].x, &texcoords[0].x, texid);
-//		}
-//		break;
-//	}
-//}
-//
-//static void _draw_end()
-//{
-//	mgr->GetContext()->Clear(0);
-//
-//	if (DRAW_END) {
-//		DRAW_END();
-//	} else {
-//		s2::RenderCtxStack::Instance()->Pop(false);
-//	}
-//}
-//
-//static void _draw_flush()
-//{
-//	sl::Shader* shader = sl::ShaderMgr::Instance()->GetShader();
-//	if (shader) {
-//		shader->Commit();
-//	}
-//}
-//
-//static void _scissor_enable() 
-//{
-//	s2::RenderScissor::Instance()->Open();
-//}
-//
-//static void _scissor_disable() 
-//{
-//	s2::RenderScissor::Instance()->Close();
-//}
-//
-//#define IS_POT(x) ((x) > 0 && ((x) & ((x) -1)) == 0)
-//
-//static int _texture_create(int type, int width, int height, const void* data, int channel,unsigned int id) {
-//	//ur::TEXTURE_FORMAT format = TEXTURE_RGBA8;
-//	//switch (type) {
-//	//case DTEX_TF_RGBA8:
-//	//	format = TEXTURE_RGBA8;
-//	//	break;
-//	//case DTEX_TF_PVR4:
-//	//	format = TEXTURE_PVR4;
-//	//	break;
-//	//default:
-//	//	assert(0);
-//	//}
-//
-//	//return RenderContext::Instance()->GetImpl()->CreateTexture(static_cast<const uint8_t*>(data), width, height, format);
-//
-//	return 0;
-//}
-//
-//static void 
-//_texture_release(int id) {
-//	RenderContext::Instance()->GetImpl()->ReleaseTexture(id);
-//}
-//
-//static void
-//_texture_update(const void* pixels, int w, int h, unsigned int id) {
-//	RenderContext::Instance()->GetImpl()->UpdateTexture(static_cast<const uint8_t*>(pixels), w, h, id);
-//}
-//
-//static void
-//_sub_tex_update(const void* pixels, int x, int y, int w, int h, unsigned int id) {
-//	RenderContext::Instance()->GetImpl()->UpdateSubTexture(static_cast<const uint8_t*>(pixels), x, y, w, h, id);
-//}
-//
-//static int
-//_texture_id(int id) {
-//	return id;
-//}
-//
-//static void 
-//_clear_color(float xmin, float ymin, float xmax, float ymax) {
-////	render_gl_blend_disable();
-//////	glBlendFunc(GL_ONE, GL_ZERO);
-////
-////	sl::ShaderMgr* mgr = sl::ShaderMgr::Instance();
-////
-////	mgr->SetShader(sl::SHAPE2);
-////	sl::ShapeShader* shader = static_cast<sl::ShapeShader*>(mgr->GetShader());
-////	
-////	shader->SetColor(0);
-//////	shader->SetColor(0xff0000ff);
-////
-////	int x, y, w, h;
-////	dtex_gl_get_viewport(&x, &y, &w, &h);
-////	xmin = w * 0.5f * (xmin - 1);
-////	xmax = w * 0.5f * (xmax - 1);
-////	ymin = h * 0.5f * (ymin + 1);
-////	ymax = h * 0.5f * (ymax + 1);
-////
-////	std::vector<sm::vec2> triangles(4);
-////	triangles[0].Set(xmin, ymin);
-////	triangles[1].Set(xmin, ymax);
-////	triangles[2].Set(xmax, ymin);
-////	triangles[3].Set(xmax, ymax);
-////
-////	s2::RVG::TriangleStrip(triangles);
-////
-////	shader->Commit();
-////// 	ShaderLab::Instance()->Flush();
-////
-//////	glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
-////	render_gl_blend_enable();
-////
-////	//////////////////////////////////////////////////////////////////////////
-////
-//////	dtex_gl_clear_color(0, 0, 0, 0);
-//}
-//
-////////////////////////////////////////////////////////////////////////////
-//
-//static const char* CFG =
-//	"{ \n"
-//	"	\"open_c1\" : true,		\n"
-//	"	\"open_c2\" : true,		\n"
-//	"	\"open_c3\" : false,	\n"
-//	"	\"open_c4\" : false,	\n"
-//	"	\"open_cg\" : true,		\n"
-//	"	\"open_cs\" : true,		\n"
-//	"	\"c1_tex_size\" : 2048, \n"
-//	"	\"c2_tex_size\" : 2048	\n"
-//	"} \n"
-//	;
-//
-//DTex::DTex()
-//{
-//// 	dtex_shader_init(&_program, &_blend, &_set_texture, &_get_texture, &_set_target, &_get_target,
-//// 		&_draw_begin, &_draw, &_draw_end, &_draw_flush, &_scissor_enable, _scissor_disable);
-//// 
-//// 	dtex_gl_init(&_clear_color);
-//// 	dtex_gl_texture_init(&_texture_create, &_texture_release, &_texture_update, &_sub_tex_update, &_texture_id);
-//// 
-//// 	dtexf_create(CFG);
-//}
-//
-//void DTex::InitHook(void (*draw_begin)(), void (*draw_end)())
-//{
-//	DRAW_BEGIN = draw_begin;
-//	DRAW_END = draw_end;
-//}
-//
-//void DTex::LoadBegin()
-//{
-////	dtexf_c2_load_begin();
-//}
-//
-//int DTex::Load(const std::string& filepath, const s2::Texture* tex)
-//{
-//// 	std::map<std::string, int>::iterator itr = m_map_filepath.find(filepath);
-//// 	if (itr != m_map_filepath.end()) {
-//// 		return itr->second;
-//// 	}
-//// 
-//// 	int key = m_map_filepath.size();
-//// 	m_map_filepath.insert(std::make_pair(filepath, key));
-//// 	sm::vec2 sz = tex->GetSize();
-//// 	dtexf_c2_load_tex(tex->GetTexID(), (int)sz.x, (int)sz.y, key);
-//// 
-//// 	return key;
-//
-//	return 0;
-//}
-//
-//void DTex::LoadEnd()
-//{
-////	dtexf_c2_load_end();
-//}
-//
-//float* DTex::Query(const std::string& filepath, const s2::Texture* tex, int* id)
-//{
-//// 	if (filepath.empty()) {
-//// 		return NULL;
-//// 	}
-//// 
-//// 	int key;
-//// 	std::map<std::string, int>::iterator itr = m_map_filepath.find(filepath);
-//// 	if (itr != m_map_filepath.end()) {
-//// 		key = itr->second;
-//// 	} else {
-//// 		dtexf_c2_load_begin();
-//// 		key = Load(filepath, tex);
-//// 		dtexf_c2_load_end();
-//// 	}
-//// 	return dtexf_c2_query_tex(key, id);
-//
-//	return NULL;
-//}
-//
-//void DTex::Clear()
-//{
-////	dtexf_c2_clear();
-//}
-//
-//dtex_cg* DTex::GetDtexCG()
-//{
-////	return dtexf_get_cg();
-//	return NULL;
-//}
-//
-//void DTex::OnSize(int w, int h)
-//{
-////	dtex_set_screen(static_cast<float>(w), static_cast<float>(h), 1);
-//}
-//
-//void DTex::Update()
-//{
-////	dtexf_update();
-//}
-//
-//void DTex::DebugDraw() const
-//{
-//// 	if (sl::Shader* shader = sl::ShaderMgr::Instance()->GetShader()) {
-//// 		shader->Commit();
-//// 	}
-//// 	dtexf_debug_draw();
-//}
-//
-//}
+#include "GUM_DTex.h"
+#include "ImageLoader.h"
+#include "RenderContext.h"
+#include "ProxyImage.h"
+
+#include <logger.h>
+#include <timp/Package.h>
+#include <timp/PkgMgr.h>
+#include <timp/TextureLoader.h>
+#include <simp/RelocateTexcoords.h>
+#include <dtex2/RenderAPI.h>
+#include <dtex2/ResourceAPI.h>
+#include <dtex2/Package.h>
+#include <dtex2/PkgMgr.h>
+#include <dtex2/TextureRaw.h>
+#include <dtex2/Cache.h>
+#include <dtex2/CacheMgr.h>
+#include <dtex2/CacheAPI.h>
+#include <dtex2/CacheSymbol.h>
+#include <dtex2/CS_Node.h>
+#include <shaderlab/ShaderMgr.h>
+#include <shaderlab/ShapeShader.h>
+#include <shaderlab/Sprite2Shader.h>
+#include <shaderlab/FilterShader.h>
+#include <sprite2/S2_RVG.h>
+#include <sprite2/RenderCtx.h>
+#include <sprite2/RenderCtxStack.h>
+#include <sprite2/RenderScissor.h>
+#include <unirender/RenderContext.h>
+
+#include <string>
+
+#include <assert.h>
+
+namespace dtex { class Package; }
+
+namespace gum
+{
+
+SINGLETON_DEFINITION(DTex);
+
+static void (*DRAW_BEGIN)() = NULL;
+static void (*DRAW_END)() = NULL;
+
+/************************************************************************/
+/* draw                                                                 */
+/************************************************************************/
+
+static void 
+clear_color_part(float xmin, float ymin, float xmax, float ymax)
+{
+	RenderContext::Instance()->GetImpl()->EnableBlend(false);
+//	glBlendFunc(GL_ONE, GL_ZERO);
+
+	sl::ShaderMgr* mgr = sl::ShaderMgr::Instance();
+
+	mgr->SetShader(sl::SHAPE2);
+	sl::ShapeShader* shader = static_cast<sl::ShapeShader*>(mgr->GetShader());
+	
+	shader->SetColor(0);
+//	shader->SetColor(0xff0000ff);
+
+	int w = RenderContext::Instance()->GetWidth(),
+		h = RenderContext::Instance()->GetHeight();
+	xmin = w * 0.5f * (xmin - 1);
+	xmax = w * 0.5f * (xmax - 1);
+	ymin = h * 0.5f * (ymin + 1);
+	ymax = h * 0.5f * (ymax + 1);
+
+	std::vector<sm::vec2> triangles(4);
+	triangles[0].Set(xmin, ymin);
+	triangles[1].Set(xmin, ymax);
+	triangles[2].Set(xmax, ymin);
+	triangles[3].Set(xmax, ymax);
+
+	s2::RVG::TriangleStrip(triangles);
+
+	shader->Commit();
+// 	ShaderLab::Instance()->Flush();
+
+//	glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+	RenderContext::Instance()->GetImpl()->EnableBlend(true);
+
+	//////////////////////////////////////////////////////////////////////////
+
+//	dtex_gl_clear_color(0, 0, 0, 0);
+}
+
+static void 
+set_program()
+{
+	// todo
+	sl::ShaderMgr::Instance()->SetShader(sl::SPRITE2);
+}
+
+static void 
+set_blend(int mode)
+{
+	// todo
+	assert(0);
+
+	assert(mode == 0);
+//	ShaderMgr::Instance()->SetBlendMode(0);
+}
+
+static void 
+draw_begin()
+{
+	if (DRAW_BEGIN) {
+		DRAW_BEGIN();
+	} else {
+		s2::RenderCtxStack::Instance()->Push(s2::RenderCtx(2, 2, 0, 0), false);
+	}
+
+	sl::ShaderMgr* sl_mgr = sl::ShaderMgr::Instance();
+	sl_mgr->SetShader(sl::SPRITE2);
+	sl::Sprite2Shader* sl_shader = static_cast<sl::Sprite2Shader*>(sl_mgr->GetShader());
+	sl_shader->SetColor(0xffffffff, 0);
+	sl_shader->SetColorMap(0x000000ff, 0x0000ff00, 0x00ff0000);
+}
+
+static void 
+draw(const float _vertices[8], const float _texcoords[8], int texid)
+{
+	sm::vec2 vertices[4], texcoords[4];
+	for (int i = 0; i < 4; ++i) {
+		vertices[i].x  = _vertices[i * 2];
+		vertices[i].y  = _vertices[i * 2 + 1];
+		texcoords[i].x = _texcoords[i * 2];
+		texcoords[i].y = _texcoords[i * 2 + 1];
+	}
+
+	sl::ShaderMgr* sl_mgr = sl::ShaderMgr::Instance();
+	switch (sl_mgr->GetShaderType()) 
+	{
+	case sl::SPRITE2:
+		{
+			sl::Sprite2Shader* shader = static_cast<sl::Sprite2Shader*>(sl_mgr->GetShader());
+			shader->Draw(&vertices[0].x, &texcoords[0].x, texid);
+		}
+		break;
+	case sl::FILTER:
+		{
+			sl::FilterShader* shader = static_cast<sl::FilterShader*>(sl_mgr->GetShader());
+			shader->Draw(&vertices[0].x, &texcoords[0].x, texid);
+		}
+		break;
+	}
+}
+
+static void 
+draw_end()
+{
+	sl::ShaderMgr::Instance()->FlushShader();
+
+	if (DRAW_END) {
+		DRAW_END();
+	} else {
+		s2::RenderCtxStack::Instance()->Pop(false);
+	}
+}
+
+static void 
+draw_flush()
+{
+	sl::Shader* shader = sl::ShaderMgr::Instance()->GetShader();
+	if (shader) {
+		shader->Commit();
+	}
+}
+
+/************************************************************************/
+/* Resource                                                             */
+/************************************************************************/
+
+static const std::string& 
+get_tex_filepath(int pkg_id, int tex_idx, int lod_layer) 
+{
+	const timp::Package* pkg = timp::PkgMgr::Instance()->Query(pkg_id);
+	assert(pkg);
+	return pkg->GetTexPath(tex_idx, lod_layer);
+}
+
+static void 
+load_texture(int pkg_id, int tex_idx) 
+{
+	const timp::Package* t_pkg = timp::PkgMgr::Instance()->Query(pkg_id);
+	assert(t_pkg);
+	const std::string& filepath = t_pkg->GetTexPath(tex_idx, 0);
+	
+	ImageLoader loader(filepath);
+	bool ret = loader.Load();
+	if (!ret) {
+		return;
+	}
+
+	const dtex::Package* d_pkg = dtex::PkgMgr::Instance()->Query(pkg_id);
+	dtex::Texture* tex = d_pkg->GetTexture(tex_idx);
+	assert(tex->Type() == dtex::TEX_RAW);
+	dtex::TextureRaw* raw_tex = static_cast<dtex::TextureRaw*>(tex);
+	raw_tex->SetID(loader.GetID());
+	raw_tex->SetSize(loader.GetWidth(), loader.GetHeight());
+	raw_tex->SetFormat(loader.GetFormat());
+}
+
+static void 
+load_texture_cb(int pkg_id, int tex_idx, void (*cb)(int format, int w, int h, const void* data, void* ud), void* ud) 
+{
+	const timp::Package* t_pkg = timp::PkgMgr::Instance()->Query(pkg_id);
+	assert(t_pkg);
+	const std::string& filepath = t_pkg->GetTexPath(tex_idx, 0);
+
+	timp::TextureLoader loader(filepath);
+	loader.Load();
+
+	cb(loader.GetFormat(), loader.GetWidth(), loader.GetHeight(), loader.GetData(), ud);
+}
+
+/************************************************************************/
+/* Cache                                                                */
+/************************************************************************/
+
+static void
+relocate_pkg(int src_pkg, int src_tex, int dst_tex_id, int dst_fmt, int dst_w, int dst_h, int dst_xmin, int dst_ymin, int dst_xmax, int dst_ymax)
+{
+	simp::RelocateTexcoords::Item item;
+	item.src_pkg = src_pkg;
+	item.src_tex = src_tex;
+	item.dst_tex_id = dst_tex_id;
+	item.dst_w = dst_w;
+	item.dst_h = dst_h;
+	item.dst_xmin = dst_xmin;
+	item.dst_ymin = dst_ymin;
+	item.dst_xmax = dst_xmax;
+	item.dst_ymax = dst_ymax;
+	simp::RelocateTexcoords::Instance()->Add(item);
+
+	std::string filepath = ProxyImage::GetFilepath(dst_tex_id);
+	Image* img = ImageMgr::Instance()->Query(filepath);
+	if (img) {
+		ProxyImage* p_img = static_cast<ProxyImage*>(img);
+		p_img->Init(dst_tex_id, dst_w, dst_h, dst_fmt);
+	} else {
+		img = new ProxyImage(dst_tex_id, dst_w, dst_h, dst_fmt);
+		std::string filepath = ProxyImage::GetFilepath(dst_tex_id);
+		bool succ = ImageMgr::Instance()->Add(filepath, img);
+		assert(succ);
+		img->RemoveReference();
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+void DTex::InitHook(void (*draw_begin)(), void (*draw_end)())
+{
+ 	DRAW_BEGIN = draw_begin;
+ 	DRAW_END = draw_end;
+}
+
+DTex::DTex()
+{
+	dtex::RenderAPI::Callback render_cb;
+	render_cb.clear_color_part = clear_color_part;
+	render_cb.set_program      = set_program;
+	render_cb.set_blend        = set_blend;
+	render_cb.draw_begin       = draw_begin;
+	render_cb.draw             = draw;
+	render_cb.draw_end         = draw_end;
+	render_cb.draw_flush       = draw_flush;
+
+	dtex::RenderAPI::InitCallback(render_cb);
+	dtex::RenderAPI::InitRenderContext(RenderContext::Instance()->GetImpl());
+
+	dtex::ResourceAPI::Callback res_cb;
+	res_cb.load_texture    = load_texture;
+	res_cb.load_texture_cb = load_texture_cb;
+	dtex::ResourceAPI::InitCallback(res_cb);
+
+	dtex::CacheAPI::Callback cache_cb;
+	cache_cb.relocate_pkg = relocate_pkg;
+	dtex::CacheAPI::InitCallback(cache_cb);
+
+	m_c2 = new dtex::CacheSymbol(2048, 2048, 0, 0);
+	dtex::CacheMgr::Instance()->Add(m_c2, "C2");
+}
+
+void DTex::CreatePkg(int pkg_id)
+{
+	const timp::Package* src = timp::PkgMgr::Instance()->Query(pkg_id);
+	assert(src);
+
+	dtex::Package* dst = new dtex::Package(pkg_id);
+
+	const std::vector<timp::Package::TextureDesc>& textures 
+		= src->GetAllTextures();
+	for (int i = 0, n = textures.size(); i < n; ++i) 
+	{
+		const timp::Package::TextureDesc& desc = textures[i];
+		dtex::Texture* tex = new dtex::TextureRaw(1);
+		tex->SetFormat(desc.type);
+		tex->SetSize(desc.w, desc.h);
+		dst->AddTexture(tex);
+	}
+
+	dtex::PkgMgr::Instance()->Add(dst, pkg_id);
+}
+
+void DTex::LoadSymStart()
+{
+	m_c2->LoadStart();
+}
+
+void DTex::LoadSymbol(const std::string& filepath, int tex_id, int tex_w, int tex_h)
+{
+	uint32_t key = m_file2id.Add(filepath);
+	if (key == 0xffffffff) {
+		return;
+	}
+
+	dtex::Rect r;
+	r.xmin = r.ymin = 0;
+	r.xmax = tex_w;
+	r.ymax = tex_h;
+	m_c2->Load(tex_id, tex_w, tex_h, r, key);
+}
+
+void DTex::LoadSymFinish()
+{
+	m_c2->LoadFinish();
+}
+
+const float* DTex::QuerySymbol(const std::string& filepath, int* tex_id) const
+{
+	uint32_t key = m_file2id.Query(filepath);
+	const dtex::CS_Node* node = m_c2->Query(key);
+	if (node) {
+		*tex_id = m_c2->GetTexID();
+		return node->GetTexcoords();
+	} else {
+		return NULL;
+	}
+}
+
+void DTex::Clear()
+{
+	// todo
+	// dtexf_c2_clear();
+}
+
+void DTex::DebugDraw() const
+{
+	const std::map<std::string, dtex::Cache*>& caches 
+		= dtex::CacheMgr::Instance()->FetchAll();
+	std::map<std::string, dtex::Cache*>::const_iterator itr = caches.begin();
+	for ( ; itr != caches.end(); ++itr) {
+		itr->second->DebugDraw();
+	}
+}
+
+/************************************************************************/
+/* struct DTex::Filepath2ID                                            */
+/************************************************************************/
+
+uint32_t DTex::Filepath2ID::
+Add(const std::string& filepath)
+{
+	std::map<std::string, int>::iterator itr0 = map_filepath.find(filepath);
+	if (itr0 != map_filepath.end()) {
+		return 0xffffffff;
+	}
+
+	uint32_t hash = HashStr(filepath);
+	map_filepath.insert(std::make_pair(filepath, hash));
+
+	std::set<int>::iterator itr1 = set_id.find(hash);
+	if (itr1 == set_id.end()) {
+		set_id.insert(hash);
+	} else {
+		std::map<std::string, int>::iterator itr = conflict.find(filepath);
+		assert(itr == conflict.end());
+
+		hash = 0x7FFFFFFF + conflict.size();
+		conflict.insert(std::make_pair(filepath, hash));
+	}
+
+	return hash;
+}
+
+uint32_t DTex::Filepath2ID::
+Query(const std::string& filepath) const
+{
+	uint32_t hash = HashStr(filepath);
+	std::map<std::string, int>::const_iterator itr 
+		= conflict.find(filepath);
+	if (itr != conflict.end()) {
+		return itr->second;
+	} else {
+		return hash;
+	}
+}
+
+uint32_t DTex::Filepath2ID::
+HashStr(const std::string& _str)
+{
+	const char* str = _str.c_str();
+
+	// BKDR Hash Function
+	unsigned int seed = 131; // 31 131 1313 13131 131313 etc..
+	unsigned int hash = 0;
+
+	while (*str) {
+		hash = hash * seed + (*str++);
+	}
+
+	return (hash & 0x7FFFFFFF);
+}
+
+}
