a903675a2985d03bd6b86cab2042e84fa921ce5d
zz <zhuguang@ejoy.com>
2017-01-03 17:23:49 +0800

    [FIXED] s2 debug draw

--- source/adapter/GUM_Sprite2.cpp
+++ source/adapter/GUM_Sprite2.cpp
@@ -1,5 +1,6 @@
 #include "GUM_Sprite2.h"
 
+#include <sprite2/RenderTargetMgr.h>
 #include <sprite2/RenderTarget.h>
 #include <dtex2/DebugDraw.h>
 
@@ -14,11 +15,12 @@ Sprite2::Sprite2()
 
 void Sprite2::DebugDraw() const
 {
-	s2::RenderTarget* RT = s2::RenderTarget::Instance();
-	dtex::DebugDraw::Draw(RT->GetTexID(0), 1);
-	dtex::DebugDraw::Draw(RT->GetTexID(1), 2);
-	dtex::DebugDraw::Draw(RT->GetTexID(2), 3);
-	dtex::DebugDraw::Draw(RT->GetTexID(3), 4);
+	s2::RenderTargetMgr* RT = s2::RenderTargetMgr::Instance();
+	s2::RenderTarget* rt = RT->Fetch();
+	if (rt) {
+		dtex::DebugDraw::Draw(rt->GetTexID(), 1);
+		RT->Return(rt);
+	}
 }
 
 }
