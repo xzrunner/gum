acfcb5fb970ad9d7409a7071de07a7629888aea3
zz <zhuguang@ejoy.com>
2017-01-04 16:26:30 +0800

    [ADDED] c2 strategy

---
+++ include/gum/DTexC2Strategy.h
@@ -0,0 +1,84 @@
+#ifndef _GUM_DTEX_C2_STRATEGY_H_
+#define _GUM_DTEX_C2_STRATEGY_H_
+
+#include <SM_Rect.h>
+#include <CU_Singleton.h>
+
+#include <map>
+
+#include <stdint.h>
+
+namespace gum
+{
+
+class DTexC2Strategy
+{
+public:
+	void OnC2QueryFail(uint32_t id, int tex_id, int tex_w, int tex_h, const sm::ui16_rect& region);
+
+	void Update();
+
+	void Clear();
+	
+private:
+	struct Symbol
+	{
+		int16_t count;
+
+		int16_t tex_id;
+		int16_t tex_w, tex_h;
+		
+		sm::ui16_rect region;
+
+		Symbol() : count(0), tex_id(0), tex_w(0), tex_h(0) {}
+	};
+
+	class Package
+	{
+	public:
+		Package(int pkg_id, int sym_count);
+		~Package();
+
+		void AddCount(int id, int tex_id, int tex_w, int tex_h, const sm::ui16_rect& region);
+
+		int GetSingleMaxCount() const { return m_single_max_count; }
+		int GetDiffSprCount() const { return m_diff_spr_count; }
+		int GetTotCount() const { return m_tot_count; }
+
+		void Load();
+
+		void Clear();
+	
+	private:
+		int m_pkg_id;
+
+		int m_single_max_count;
+		int m_diff_spr_count;
+		int m_tot_count;
+
+		int m_sym_count;
+		Symbol* m_symbols;
+
+	}; // Package
+
+private:
+	void LoadPackage(Package* pkg);
+
+private:
+	std::map<int, Package*> m_pkgs;
+
+	int m_max_no_update_count;
+	int m_no_update_count;
+	float m_discount;
+
+	int m_single_max_count;
+	int m_diff_spr_count;
+	int m_tot_count;
+
+	SINGLETON_DECLARATION(DTexC2Strategy)
+
+}; // DTexC2Strategy
+
+}
+
+#endif // _GUM_DTEX_C2_STRATEGY_H_
---
+++ source/adapter/DTexC2Strategy.cpp
@@ -0,0 +1,139 @@
+#include "DTexC2Strategy.h"
+#include "GUM_DTex.h"
+
+#include <simp/NodeID.h>
+#include <simp/NodeFactory.h>
+
+namespace gum
+{
+
+SINGLETON_DEFINITION(DTexC2Strategy);
+
+DTexC2Strategy::DTexC2Strategy()
+{
+	m_max_no_update_count = 128;
+	m_no_update_count = 0;
+	m_discount = 1;
+
+//	m_single_max_count	= 512;
+	m_single_max_count	= 5;
+	m_diff_spr_count	= 256;
+	m_tot_count			= 10240;
+}
+
+void DTexC2Strategy::OnC2QueryFail(uint32_t id, int tex_id, int tex_w, int tex_h, const sm::ui16_rect& region)
+{
+	Package* pkg = NULL;
+	int pkg_id = simp::NodeID::GetPkgID(id);
+	int node_id = simp::NodeID::GetNodeID(id);
+	std::map<int, Package*>::iterator itr = m_pkgs.find(pkg_id);
+	if (itr != m_pkgs.end()) {
+		pkg = itr->second;
+	} else {
+		const simp::Package* p = simp::NodeFactory::Instance()->QueryPkg(id);
+		assert(p);
+		pkg = new Package(pkg_id, p->GetMaxNodeID() + 1);
+		m_pkgs.insert(std::make_pair(pkg_id, pkg));
+	}
+	pkg->AddCount(node_id, tex_id, tex_w, tex_h, region);
+
+	if (pkg->GetSingleMaxCount() > m_single_max_count) {
+		LoadPackage(pkg);
+	} else if (pkg->GetDiffSprCount() > m_diff_spr_count) {
+		LoadPackage(pkg);
+	} else if (pkg->GetTotCount() > m_tot_count) {
+		LoadPackage(pkg);
+	}
+}
+
+void DTexC2Strategy::Update()
+{
+	++m_no_update_count;
+	if (m_no_update_count <= m_max_no_update_count) {
+		m_discount = (float)(m_max_no_update_count - m_no_update_count) / m_max_no_update_count;
+	}
+}
+
+void DTexC2Strategy::Clear()
+{
+	std::map<int, Package*>::iterator itr = m_pkgs.begin();
+	for ( ; itr != m_pkgs.end(); ++itr) {
+		delete itr->second;
+	}
+	m_pkgs.clear();
+}
+
+void DTexC2Strategy::LoadPackage(Package* pkg)
+{
+	pkg->Load();
+	pkg->Clear();
+
+	m_no_update_count = 0;
+	m_discount = 1;
+}
+
+/************************************************************************/
+/* class DTexC2Strategy::Package                                        */
+/************************************************************************/
+
+DTexC2Strategy::Package::
+Package(int pkg_id, int sym_count)
+	: m_pkg_id(pkg_id)
+	, m_single_max_count(0)
+	, m_diff_spr_count(0)
+	, m_tot_count(0)
+{
+	m_sym_count = sym_count;
+	m_symbols = new Symbol[m_sym_count];
+}
+
+DTexC2Strategy::Package::
+~Package()
+{
+	delete[] m_symbols;
+}
+
+void DTexC2Strategy::Package::
+AddCount(int id, int tex_id, int tex_w, int tex_h, const sm::ui16_rect& region)
+{
+	++m_tot_count;
+	if (m_symbols[id].count == 0) {
+		++m_diff_spr_count;
+		m_symbols[id].tex_id = tex_id;
+		m_symbols[id].tex_w = tex_w;
+		m_symbols[id].tex_h = tex_h;
+		m_symbols[id].region = region;
+	}
+	++m_symbols[id].count;
+	if (m_symbols[id].count > m_single_max_count) {
+		m_single_max_count = m_symbols[id].count;
+	}
+}
+
+void DTexC2Strategy::Package::
+Load()
+{
+	DTex* dtex = DTex::Instance();
+	dtex->LoadSymStart();
+	for (int i = 0; i < m_sym_count; ++i) 
+	{
+		if (m_symbols[i].count == 0) {
+			continue;
+		}
+		uint32_t id = simp::NodeID::ComposeID(m_pkg_id, i);
+		UID uid = ResourceUID::BinNode(id);
+		dtex->LoadSymbol(uid, m_symbols[i].tex_id, m_symbols[i].tex_w, m_symbols[i].tex_h, m_symbols[i].region);
+	}		
+	dtex->LoadSymFinish();
+}
+
+void DTexC2Strategy::Package::
+Clear()
+{
+	m_single_max_count = m_diff_spr_count = m_tot_count = 0;
+	for (int i = 0; i < m_sym_count; ++i) {
+		m_symbols[i].count = 0;
+	}
+}
+
+}
