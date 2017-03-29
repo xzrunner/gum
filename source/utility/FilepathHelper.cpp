2c9aa5a57e2ca0e28da28088b039651dc7d4586d
zixun <lvzixun@gmail.com>
2017-03-29 16:41:57 +0800

    fix filepath helper

--- source/utility/FilepathHelper.cpp
+++ source/utility/FilepathHelper.cpp
@@ -1,47 +1,71 @@
 #include "FilepathHelper.h"
 #include "StringHelper.h"
 
-#include <boost/filesystem/operations.hpp>
-#include <boost/algorithm/string.hpp>
+#ifndef _LR_CLIENT
+	#include <boost/filesystem/operations.hpp>
+	#include <boost/algorithm/string.hpp>
+#endif
+
+#include <assert.h>
 
 namespace gum
 {
 
 std::string FilepathHelper::Relative(const std::string& base, const std::string& path)
 {
-	return boost::filesystem::relative(path, base).string();
+	#ifndef _LR_CLIENT
+		return boost::filesystem::relative(path, base).string();
+	#else
+		assert(0);
+	#endif
 }
 
 std::string FilepathHelper::Absolute(const std::string& base, const std::string& path)
 {
-	return boost::filesystem::absolute(path, base).string();
+	#ifndef _LR_CLIENT
+		return boost::filesystem::absolute(path, base).string();
+	#else
+		assert(0);
+	#endif;
 }
 
 std::string FilepathHelper::Dir(const std::string& path)
 {
-	return boost::filesystem::path(path).parent_path().string();
+	#ifndef _LR_CLIENT
+		return boost::filesystem::path(path).parent_path().string();
+	#else
+		assert(0);
+	#endif;
 }
 
 std::string FilepathHelper::Format(const std::string& path)
 {	
-	std::string ret = path;
+	#ifndef _LR_CLIENT
+		std::string ret = path;
 
-	if (Exists(ret)) {
-		ret = boost::filesystem::canonical(ret).string();
-	}
+		if (Exists(ret)) {
+			ret = boost::filesystem::canonical(ret).string();
+		}
 
-//	char from = '\\', to = '/';
-	char from = '/', to = '\\';
-	std::replace(ret.begin(), ret.end(), from, to);
+	//	char from = '\\', to = '/';
+		char from = '/', to = '\\';
+		std::replace(ret.begin(), ret.end(), from, to);
 
-	StringHelper::ToLower(ret);
+		StringHelper::ToLower(ret);
 
-	return ret;
+		return ret;
+	#else
+		assert(0);
+	#endif
 }
 
 bool FilepathHelper::Exists(const std::string& path)
 {
-	return boost::filesystem::exists(path);
+	#ifndef _LR_CLIENT
+		return boost::filesystem::exists(path);
+	#else
+		assert(0);
+	#endif
 }
 
 }
