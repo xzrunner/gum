fe935e7f7e5815c62295e37502bc55a8405d4fbf
zz <zhuguang@ejoy.com>
2017-03-29 21:43:24 +0800

    rename

--- source/utility/FilepathHelper.cpp
+++ source/utility/FilepathHelper.cpp
@@ -1,7 +1,7 @@
 #include "FilepathHelper.h"
 #include "StringHelper.h"
 
-#ifndef _LR_CLIENT
+#ifndef NO_BOOST
 	#include <boost/filesystem/operations.hpp>
 	#include <boost/algorithm/string.hpp>
 #endif
@@ -13,59 +13,64 @@ namespace gum
 
 std::string FilepathHelper::Relative(const std::string& base, const std::string& path)
 {
-	#ifndef _LR_CLIENT
-		return boost::filesystem::relative(path, base).string();
-	#else
-		assert(0);
-	#endif
+#ifndef NO_BOOST
+	return boost::filesystem::relative(path, base).string();
+#else
+	assert(0);
+	return "";
+#endif // NO_BOOST
 }
 
 std::string FilepathHelper::Absolute(const std::string& base, const std::string& path)
 {
-	#ifndef _LR_CLIENT
-		return boost::filesystem::absolute(path, base).string();
-	#else
-		assert(0);
-	#endif;
+#ifndef NO_BOOST
+	return boost::filesystem::absolute(path, base).string();
+#else
+	assert(0);
+	return "";
+#endif // NO_BOOST
 }
 
 std::string FilepathHelper::Dir(const std::string& path)
 {
-	#ifndef _LR_CLIENT
-		return boost::filesystem::path(path).parent_path().string();
-	#else
-		assert(0);
-	#endif;
+#ifndef NO_BOOST
+	return boost::filesystem::path(path).parent_path().string();
+#else
+	assert(0);
+	return "";
+#endif // NO_BOOST
 }
 
 std::string FilepathHelper::Format(const std::string& path)
 {	
-	#ifndef _LR_CLIENT
-		std::string ret = path;
+#ifndef NO_BOOST
+	std::string ret = path;
 
-		if (Exists(ret)) {
-			ret = boost::filesystem::canonical(ret).string();
-		}
+	if (Exists(ret)) {
+		ret = boost::filesystem::canonical(ret).string();
+	}
 
-	//	char from = '\\', to = '/';
-		char from = '/', to = '\\';
-		std::replace(ret.begin(), ret.end(), from, to);
+//	char from = '\\', to = '/';
+	char from = '/', to = '\\';
+	std::replace(ret.begin(), ret.end(), from, to);
 
-		StringHelper::ToLower(ret);
+	StringHelper::ToLower(ret);
 
-		return ret;
-	#else
-		assert(0);
-	#endif
+	return ret;
+#else
+	assert(0);
+	return "";
+#endif // NO_BOOST
 }
 
 bool FilepathHelper::Exists(const std::string& path)
 {
-	#ifndef _LR_CLIENT
-		return boost::filesystem::exists(path);
-	#else
-		assert(0);
-	#endif
+#ifndef NO_BOOST
+	return boost::filesystem::exists(path);
+#else
+	assert(0);
+	return "";
+#endif // NO_BOOST
 }
 
 }
