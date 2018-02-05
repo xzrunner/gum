0c9385083f2701e2fa99b673da4e2264105b6b79
zz <zhuguang@ejoy.com>
2018-02-06 05:45:18 +0800

    rename

--- include/gum/trans_color.h
+++ include/gum/trans_color.h
@@ -3,17 +3,17 @@
 
 #include <painting2/Color.h>
 #include <cu/cu_stl.h>
-#include <sns/ColorParser.h>
+#include <s2s/ColorParser.h>
 
 namespace gum
 {
 
-pt2::Color str2color(const CU_STR& str, sns::PIXEL_TYPE type);
-CU_STR color2str(const pt2::Color& col, sns::PIXEL_TYPE type);
+pt2::Color str2color(const CU_STR& str, s2s::PIXEL_TYPE type);
+CU_STR color2str(const pt2::Color& col, s2s::PIXEL_TYPE type);
 
-pt2::Color int2color(uint32_t i, sns::PIXEL_TYPE type);
-uint32_t color2int(const pt2::Color& col, sns::PIXEL_TYPE type);
-uint32_t color2int(const uint8_t rgba[4], sns::PIXEL_TYPE type);
+pt2::Color int2color(uint32_t i, s2s::PIXEL_TYPE type);
+uint32_t color2int(const pt2::Color& col, s2s::PIXEL_TYPE type);
+uint32_t color2int(const uint8_t rgba[4], s2s::PIXEL_TYPE type);
 
 }
 
--- source/trans_color.cpp
+++ source/trans_color.cpp
@@ -6,33 +6,33 @@
 namespace gum
 {
 
-pt2::Color str2color(const CU_STR& str, sns::PIXEL_TYPE type)
+pt2::Color str2color(const CU_STR& str, s2s::PIXEL_TYPE type)
 {
 	pt2::Color ret;
-	ret.FromRGBA(sns::ColorParser::StringToRGBA(str.c_str(), str.size(), type));
+	ret.FromRGBA(s2s::ColorParser::StringToRGBA(str.c_str(), str.size(), type));
 	return ret;
 }
 
-CU_STR color2str(const pt2::Color& col, sns::PIXEL_TYPE type)
+CU_STR color2str(const pt2::Color& col, s2s::PIXEL_TYPE type)
 {
-	return sns::ColorParser::RGBAToString(col.ToRGBA(), type).c_str();
+	return s2s::ColorParser::RGBAToString(col.ToRGBA(), type).c_str();
 }
 
-pt2::Color int2color(uint32_t i, sns::PIXEL_TYPE type)
+pt2::Color int2color(uint32_t i, s2s::PIXEL_TYPE type)
 {
 	pt2::Color col;
-	col.FromRGBA(sns::ColorParser::Trans(i, type, sns::RGBA));
+	col.FromRGBA(s2s::ColorParser::Trans(i, type, s2s::RGBA));
 	return col;
 }
 
-uint32_t color2int(const pt2::Color& col, sns::PIXEL_TYPE type)
+uint32_t color2int(const pt2::Color& col, s2s::PIXEL_TYPE type)
 {
-	return sns::ColorParser::Trans(col.ToRGBA(), sns::RGBA, type);
+	return s2s::ColorParser::Trans(col.ToRGBA(), s2s::RGBA, type);
 }
 
-uint32_t color2int(const uint8_t rgba[4], sns::PIXEL_TYPE type)
+uint32_t color2int(const uint8_t rgba[4], s2s::PIXEL_TYPE type)
 {
-	return sns::ColorParser::Trans(pt2::Color(rgba[0], rgba[1], rgba[2], rgba[3]).ToRGBA(), sns::RGBA, type);
+	return s2s::ColorParser::Trans(pt2::Color(rgba[0], rgba[1], rgba[2], rgba[3]).ToRGBA(), s2s::RGBA, type);
 }
 
 }
